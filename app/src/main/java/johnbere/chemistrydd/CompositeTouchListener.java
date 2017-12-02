4bdd48f293f8f54ca0baca1dc3acb91deac2d3f7
JohnathanBere <johnbere@live.co.uk>
2017-12-02 13:48:43 +0000

    Updated .gitignore file

---
+++ app/src/main/java/johnbere/chemistrydd/CompositeTouchListener.java
@@ -0,0 +1,10 @@
+package johnbere.chemistrydd;
+
+import android.view.View;
+
+/**
+ * Created by John Bere on 30/11/2017.
+ */
+
+public class CompositeTouchListener implements View.OnTouchListener {
+}
---
+++ app/src/main/java/johnbere/chemistrydd/EventListeners.java
@@ -0,0 +1,8 @@
+package johnbere.chemistrydd;
+
+/**
+ * Created by John Bere on 30/11/2017.
+ */
+
+public class EventListeners {
+}
